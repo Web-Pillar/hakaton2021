d3ab9a9c56ea8b2dc483f05a2600b54234dab259
Meriton Ademi <81649935+Personal-Meriton@users.noreply.github.com>
2021-11-30 23:21:58 +0100

    Hakaton2021

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.config.productionTip = false
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
