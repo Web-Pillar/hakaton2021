b8636243698139957304d42fd6672e13bf60af96
Xhafer Durmishi <xhaferdurmishi95@gmail.com>
2021-11-30 23:53:48 +0100

    fixed marker icon problem

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
-Vue.config.productionTip = false
-Vue.component('l-map', LMap);
-Vue.component('l-tile-layer', LTileLayer);
-Vue.component('l-marker', LMarker);
+// The lines below fix the problem with marker icon
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
