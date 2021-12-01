1b1f0b388b97159fc09064451e18a1c462716829
Xhafer Durmishi <xhaferdurmishi95@gmail.com>
2021-12-01 15:22:47 +0100

    added schools in map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,13 @@
 <template>
   <l-map class="map-container-custom" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
+    <l-marker v-for="school in schools" :key="`school_${school.id}`" :lat-lng="[school.latitude, school.longitude]"></l-marker>
   </l-map>
 </template>
 
 <script>
 import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import { schools } from './../data/schools_v2'
 
 export default {
   components: {
@@ -19,9 +20,9 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [42.008920, 20.975204],
-      markerLatLng: [42.008920, 20.975204]
+      zoom: 10,
+      center: [41.9981, 21.4254],
+      schools: schools.filter(s => Boolean(s.latitude) && Boolean(s.longitude)),
     };
   }
 }
