8bda128ed28b6589c456f0da12cc08012dbbfc2a
besartsherifi <59452085+besartsherifi@users.noreply.github.com>
2021-12-02 00:34:06 +0100

    Form.vue
    Created component Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
     <div class="home">
       <Map msg="Welcome to Your Vue.js App"/>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -16,12 +17,14 @@
 // @ is an alias to /src
 import Map from '@/components/Map.vue';
 import Filter1 from '../components/Filter.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
     Map,
-    Filter1
+    Filter1,
+    Footer
 }
 }
 </script>
