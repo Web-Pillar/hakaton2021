e31a6134f5dfa676c8f64d4f8de510af69dc90c5
besartsherifi <59452085+besartsherifi@users.noreply.github.com>
2021-12-02 02:45:33 +0100

    Footer
    Footer Update images , logos , content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,25 @@
 <template>
 
-    <footer class="footer1">
+
+   <footer class="footer1">
 
         <div
-          class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
-          <img src="@/assets/usaid1.png">
-          
+          class="image1">
+          <img src="@/assets/usaid1.png" style="width:600px;height:350px;margin-bottom:42px;">
+          <img src="@/assets/meta.svg"   style="width:600px;height:400px;margin-bottom:42px;">
+          <img src="@/assets/Logo.png" style="width:500px;height:300px; padding-left:42px; margin-bottom:42px;">
 
 
         </div>
-        <div
-          class="flex-shrink-0 w-64 pt-14 pb-14 mx-auto text-center md:mx-0 md:text-left">
-          
-          <img src="@/assets/meta.svg">
+        <div class="list">
+            <ul class="list1">
+              <li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">About us</a></li>
+              
+            </ul>
 
 
         </div>
@@ -29,11 +36,38 @@ export default {
 </script>
 
 <style>
- .footer1{
-  position: relative;
-  padding-top: 8;
-  padding-bottom: 8;
 
- };
+ .footer1{
+  
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  
+
+
+ }
+.image1{
+   
+   align-items: center;
+}
+ .list1{
+     width: 100px;
+     font-weight: bold;
+     display: block;
+     padding-top: 2rem;
+     padding-left:6rem;
+     list-style-type: none;
+    
+     
+ }
+ .list1 a{
+     text-decoration: none;
+     color: black;
+     font-size: 1.5em;
+     
+     
+
+ } 
+
 
 </style>
