74712cdd02045681d6bad95ab2e2027c2e5f0943
Xhafer Durmishi <xhaferdurmishi95@gmail.com>
2022-02-06 16:03:06 +0100

    faq refactor

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,76 +1,76 @@
 <template>
-    <div class="faqstyle">
-    <h1 class="text-center">
-      Frequently askend questions!
-    </h1>
-  
-
-<v-container class="faqbox" >
-
-  
-
-  <v-row no-gutters >
-      <i class="ri-question-line"></i>
-    <v-col md="6">
-         
-     -What is metaschools used for?
-    </v-col>
-    
-    <v-col md="6">
-    -Metsascholl is project by metamorphis usaid and webpillar.
-    </v-col>
-  
-   <v-col md="6">
-    -What is metaschools used for?
-    </v-col>
-    <v-col md="6">
-     -Metsascholl is project by metamorphis usaid and webpillar.
-    </v-col>
-  
-   <v-col md="6">
-     -What is metaschools used for?
-    </v-col>
-    <v-col md="6">
-     -Metsascholl is project by metamorphis usaid and webpillar.
-    </v-col>
-  </v-row> 
-</v-container>
-
+  <div class="faqstyle">
+    <h1 class="text-center">Frequently askend questions!</h1>
+    <v-container class="faqbox">
+      <v-row no-gutters>
+        <v-col md="6">
+          <v-list>
+            <v-list-item v-for="(item, index) in questions" :key="`question_${index}`">
+              <v-icon>mdi-help-circle</v-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.question }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+        <v-col md="6">
+          <v-list>
+            <v-list-item v-for="(item, index) in questions" :key="`answer_${index}`">
+              <v-list-item-content>
+                <v-list-item-title>{{ item.answer }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
-  
-
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      questions: [
+        {
+          question: 'What is metaschools used for',
+          answer: 'Metsascholl is project by metamorphis usaid and webpillar',
+        },
+        {
+          question: 'What is metaschools used for',
+          answer: 'Metsascholl is project by metamorphis usaid and webpillar',
+        },
+        {
+          question: 'What is metaschools used for',
+          answer: 'Metsascholl is project by metamorphis usaid and webpillar',
+        },
+      ],
+    }
+  },
+}
+</script>
+
 <style>
-
-
-
 .faqstyle {
-    background-color: #f1f1f1;
-    padding: 30px;
-  
-    border: solid;
-   
+  background-color: #f1f1f1;
+  padding: 30px;
+
+  border: solid;
 }
 
 body {
-  
-    font-family: 'Just Another Hand', cursive;
-    background: #f3f3f3;
+  font-family: "Just Another Hand", cursive;
+  background: #f3f3f3;
 }
 
 .faqbox {
-    padding: 4rem 8rem;
-   
-    border: 3px solid #333333;
-    font-size: 1.3rem;
-    border-radius: 2% 6% 5% 4% / 1% 1% 2% 4%;
-    text-transform: uppercase;
-    letter-spacing: 0.3ch;
-    background: #ffffff;
-    
-    
-  
+  padding: 4rem 8rem;
+
+  border: 3px solid #333333;
+  font-size: 1.3rem;
+  border-radius: 2% 6% 5% 4% / 1% 1% 2% 4%;
+  text-transform: uppercase;
+  letter-spacing: 0.3ch;
+  background: #ffffff;
 }
-
-
 </style>
