5aeddd89c458fd06b8417fd32ebed736c598e493
besartsinani <61848461+besartsinani@users.noreply.github.com>
2022-03-16 15:27:34 +0100

    new about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,55 +1,32 @@
 <template>
-  <v-app id="inspire">
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="text-h4 font-weight-thin mb-4">
+       Metaschool
+        </h1>
+        <h4 class="subheading">
+          Metaschool u krijua me idenë për t’ia lehtësuar punën prindërve në zgjedhjen e arsimimit për të vegjëlit(ose fëmijët e tyre) si dhe për studentët që duan të zgjedhin më të mirën për veten e tyre. Prindërit ose studentët mund të shfrytezojnë këtë web faqe për të parë performancat dhe kushtet shkollave si psh. Laboratoret, paisjet në dispozicion dhe gjithashtu të bëj krahasim për të vendosur opsionin më të mirë. Gjithashtu mund të shohin vendndodhjen e kazinove(lojrat e fatit), të cilat gjenden pranë shkollave të shpeshta në vendin tonë dhe me ndikim të madh edhe negativ. 
+Ideja dhe realizimi ishte nga ekipi i një kompanie startup e quajtur Web-pillar. Pjesë e ekipit ishin antarë të motivuar dhe të gatshëm për punë si rezultat i të cilës ishte edhe marrja e vendit të parë në këtë hakaton. Hakaton i cili u organizua dhe u financua nga Fondacioni Metamorphosis dhe USAID për të gjithë studentët, kompanitë startup të IT-së dhe të gjithë entuziastët e IT-së
 
 
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
+        </h4>
+      
+      </v-col>
+      
+    </v-row>
+  </v-parallax>
 
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                  link
-                  color="grey lighten-4"
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  
 </template>
 
-<script>
 
-</script>
