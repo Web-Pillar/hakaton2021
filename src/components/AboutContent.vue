f5f88382bdebf057136ae2ab445b5bb0c9ebdd09
besartsinani <61848461+besartsinani@users.noreply.github.com>
2022-03-19 15:42:38 +0100

    newdesingabout

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="py-6 py-lg-12 text-center">
+    <h1 class="text-h2 text-sm-h2 text-md-h2 text-lg-h2">Every information about schools in North Macedonia starts at <span class="primary--text">Metaschools</span></h1>
+    <p class=" text-justify text-sm-h5 secondary--text mt-7 w-full w-md-8-12 w-xl-half mx-auto">Metaschool u krijua me idenë për t’ia lehtësuar punën prindërve në zgjedhjen e arsimimit për të vegjëlit(ose fëmijët e tyre) si dhe për studentët që duan të zgjedhin më të mirën për veten e tyre. Prindërit ose studentët mund të shfrytezojnë këtë web faqe për të parë performancat dhe kushtet shkollave si psh. Laboratoret, paisjet në dispozicion dhe gjithashtu të bëj krahasim për të vendosur opsionin më të mirë. Gjithashtu mund të shohin vendndodhjen e kazinove(lojrat e fatit), të cilat gjenden pranë shkollave të shpeshta në vendin tonë dhe me ndikim të madh edhe negativ. 
+Ideja dhe realizimi ishte nga ekipi i një kompanie startup e quajtur Web-pillar. Pjesë e ekipit ishin antarë të motivuar dhe të gatshëm për punë si rezultat i të cilës ishte edhe marrja e vendit të parë në këtë hakaton. Hakaton i cili u organizua dhe u financua nga Fondacioni Metamorphosis dhe USAID për të gjithë studentët, kompanitë startup të IT-së dhe të gjithë entuziastët e IT-së
+</p>
+    <div class="mt-4">
+      <v-btn x-large class="my-1 mx-sm-1 w-full w-sm-auto" color="primary">Open School Map</v-btn>
+      
+    </div>
+  </v-container>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,16 @@
 <template>
-  <v-parallax
-    dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        <h1 class="text-h4 font-weight-thin mb-4">
-       Metaschool
-        </h1>
-        <h4 class="subheading">
-          Metaschool u krijua me idenë për t’ia lehtësuar punën prindërve në zgjedhjen e arsimimit për të vegjëlit(ose fëmijët e tyre) si dhe për studentët që duan të zgjedhin më të mirën për veten e tyre. Prindërit ose studentët mund të shfrytezojnë këtë web faqe për të parë performancat dhe kushtet shkollave si psh. Laboratoret, paisjet në dispozicion dhe gjithashtu të bëj krahasim për të vendosur opsionin më të mirë. Gjithashtu mund të shohin vendndodhjen e kazinove(lojrat e fatit), të cilat gjenden pranë shkollave të shpeshta në vendin tonë dhe me ndikim të madh edhe negativ. 
-Ideja dhe realizimi ishte nga ekipi i një kompanie startup e quajtur Web-pillar. Pjesë e ekipit ishin antarë të motivuar dhe të gatshëm për punë si rezultat i të cilës ishte edhe marrja e vendit të parë në këtë hakaton. Hakaton i cili u organizua dhe u financua nga Fondacioni Metamorphosis dhe USAID për të gjithë studentët, kompanitë startup të IT-së dhe të gjithë entuziastët e IT-së
-
-
-        </h4>
-      
-      </v-col>
-      
-    </v-row>
-  </v-parallax>
-
-  
+  <AboutContent></AboutContent>
 </template>
 
+<script>
+import AboutContent from './../components/AboutContent.vue'
+export default {
+  components:{AboutContent,}
+
+}
+</script>
+
+<style>
+
+</style>
 
